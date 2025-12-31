ed59d072961b9078ae41c1792e1676feaea8a134
jeffthecool9 <107611378+jeffthecool9@users.noreply.github.com>
2025-12-31 11:39:17 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import { ToastProvider } from "./components/Toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
+    <App />
   </React.StrictMode>
 );
